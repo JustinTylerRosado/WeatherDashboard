a196fe3c5e66a16efb219e07934824e434d1bae3
Justin <justintylerrosado@gmail.com>
2025-04-30 10:32:33 -0400

    updated weather routes

--- Develop/server/src/routes/api/weatherRoutes.ts
+++ Develop/server/src/routes/api/weatherRoutes.ts
@@ -3,6 +3,10 @@ import fs from 'fs';
 import path from 'path';
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname  = path.dirname(__filename);
 
 const router = Router();
 const historyFilePath = path.join(__dirname, '../../../db/db.json');
